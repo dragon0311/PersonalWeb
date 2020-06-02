0787c09d8a1054af28c3abc148212209bf6ba8fd
chenyang <342324968@qq.com>
2020-06-03 00:31:25 +0800

    feat：上传前端做了一半

--- study/src/Main/ComputerW/index.tsx
+++ study/src/Main/ComputerW/index.tsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class ComputerW extends Component {
   render() {
     return (
       <div>
-        ComputerW
+        <Button onClick={this.uploadWallPaper}>上传壁纸</Button>
       </div>
     );
+  }
+
+  private uploadWallPaper = () => {
+    const PicUploader = document.createElement("input");
+    PicUploader.type = 'file';
+    PicUploader.onchange = (e: any) => {
+      const fr = new FileReader();
+      const name = e.target.files[0].name;
+      fr.readAsArrayBuffer(e.target.files[0]);
+      fr.onload = r => {
+        const result: ArrayBuffer = r.target?.result as ArrayBuffer;
+        const dataUint8Array = new Uint8Array(result);
+
+        // fetch(
+
+        // ).then(res => res.json())
+        // .then(data => {
+        //   if (data) {
+        //     const img = 'data:img/png;base64,' + data.content;
+        //   }
+        // })
+
+      }
+    }
   }
 }
 
