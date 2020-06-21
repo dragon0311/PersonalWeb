969a4480fc4b3f04ef3b4105221d07eca42414de
dragon0311 <342324968@qq.com>
2020-06-21 12:17:32 +0800

    feat: 上传壁纸功能

--- study/src/Main/ComputerW/index.tsx
+++ study/src/Main/ComputerW/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-
+import qs from 'querystring';
+import {v1} from 'uuid'
 class ComputerW extends Component {
   render() {
     return (
@@ -15,23 +16,46 @@ class ComputerW extends Component {
     PicUploader.type = 'file';
     PicUploader.onchange = (e: any) => {
       const fr = new FileReader();
+      const type = e.target.files[0].type;
       const name = e.target.files[0].name;
       fr.readAsArrayBuffer(e.target.files[0]);
       fr.onload = r => {
         const result: ArrayBuffer = r.target?.result as ArrayBuffer;
         const dataUint8Array = new Uint8Array(result);
+        const pic = this.arrayBufferToBase64(dataUint8Array);
+        const ID = v1();
 
-        // fetch(
-
-        // ).then(res => res.json())
-        // .then(data => {
-        //   if (data) {
-        //     const img = 'data:img/png;base64,' + data.content;
-        //   }
-        // })
+        fetch('/wallpaper/upload-pic', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: JSON.stringify({
+            ID,
+            PicName: name,
+            content: pic,
+          })
+        }).then(res => {
+          return res.json()
+        })
+        .then(data => {
+          if (data) {
+            
+          }
+        })
 
       }
     }
+    PicUploader.click();
+  }
+
+  private arrayBufferToBase64 = (buffer: Uint8Array) => {
+    let binary = '';
+    const len = buffer.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(buffer[i]);
+    }
+    return btoa(binary);
   }
 }
 
